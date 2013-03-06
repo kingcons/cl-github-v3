dfa7d5bddf3875f59753bd8bc2b273d0b22186b4
Brit Butler <redline6561@gmail.com>
2013-03-06 15:03:12 -0500

    Packaging changes.

--- repos.lisp
+++ repos.lisp
@@ -1,8 +1,6 @@
 (defpackage #:github-repo
   (:use :cl)
-  (:import-from :cl-github #:define-github-command
-                           #:api-command
-                           #:rel-path))
+  (:import-from :cl-github #:define-github-command))
 
 (in-package #:github-repo)
 
@@ -11,46 +9,56 @@
     (:docs "Create a new repo, NAME, for the authenticated user. If ORG is provided,
 creates a new repo for the given ORGANIZATION.")
   (cl-github:booleanize-parameters parameters :has-issues :has-wiki :has-downloads)
-  (api-command (if org (rel-path "/orgs/~A/repos" org) "/user/repos")
+  (cl-github:api-command (if org
+                             (cl-github:rel-path "/orgs/~A/repos" org)
+                             "/user/repos")
                :method :post
                :body parameters))
 
 (define-github-command list-repositories (org user)
     (:docs "List repositories for the given ORG or USER. If neither is given, list
 repositories for the authenticated user.")
-  (api-command (cond (org (rel-path "/orgs/~A/repos" org))
-                     (user (rel-path "/users/~A/repos" user))
-                     (t "/user/repos"))
+  (cl-github:api-command (cond (org (cl-github:rel-path "/orgs/~A/repos" org))
+                               (user (cl-github:rel-path "/users/~A/repos" user))
+                               (t "/user/repos"))
                :method :get))
 
 (define-github-command get-repository (owner repo)
     (:docs "Get the available metadat for OWNER's REPO.")
-  (api-command (rel-path "/repos/~A/~A" owner repo) :method :get))
+  (cl-github:api-command (cl-github:rel-path "/repos/~A/~A" owner repo)
+                         :method :get))
 
 (define-github-command list-contributors (owner repo)
     (:docs "List the contributors to OWNER's REPO.")
-  (api-command (rel-path "/repos/~A/~A/contributors" owner repo) :method :get))
+  (cl-github:api-command (cl-github:rel-path "/repos/~A/~A/contributors" owner repo)
+                         :method :get))
 
 (define-github-command list-languages (owner repo)
     (:docs "List the languages used in OWNER's REPO.")
-  (api-command (rel-path "/repos/~A/~A/languages" owner repo) :method :get))
+  (cl-github:api-command (cl-github:rel-path "/repos/~A/~A/languages" owner repo)
+                         :method :get))
 
 (define-github-command list-teams (owner repo)
     (:docs "List the teams involved in OWNER's REPO.")
-  (api-command (rel-path "/repos/~A/~A/teams" owner repo) :method :get))
+  (cl-github:api-command (cl-github:rel-path "/repos/~A/~A/teams" owner repo)
+                         :method :get))
 
 (define-github-command list-tags (owner repo)
     (:docs "List the git tags for OWNER's REPO.")
-  (api-command (rel-path "/repos/~A/~A/tags" owner repo) :method :get))
+  (cl-github:api-command (cl-github:rel-path "/repos/~A/~A/tags" owner repo)
+                         :method :get))
 
 (define-github-command list-branches (owner repo)
     (:docs "List the git branches for OWNER's REPO.")
-  (api-command (rel-path "/repos/~A/~A/branches" owner repo) :method :get))
+  (cl-github:api-command (cl-github:rel-path "/repos/~A/~A/branches" owner repo)
+                         :method :get))
 
 (define-github-command delete-repository (owner repo)
     (:docs "Delete the given OWNER's REPO.")
-  (api-command (rel-path "/repos/~A/~A" owner repo) :method :delete))
+  (cl-github:api-command (cl-github:rel-path "/repos/~A/~A" owner repo)
+                         :method :delete))
 
 (define-github-command get-branch (owner repo branch)
     (:docs "Get metadata about BRANCH from OWNER's REPO.")
-  (api-command (rel-path "/repos/~A/~A/branches/~A" owner repo branch) :method :get))
+  (cl-github:api-command (cl-github:rel-path "/repos/~A/~A/branches/~A" owner repo branch)
+                         :method :get))
--- search.lisp
+++ search.lisp
@@ -1,8 +1,6 @@
 (defpackage #:github-search
   (:use :cl)
-  (:import-from :cl-github #:define-github-command
-                           #:api-command
-                           #:rel-path))
+  (:import-from :cl-github #:define-github-command))
 
 (in-package #:github-search)
 
@@ -10,18 +8,18 @@
     (:docs "Search for issues on OWNER's REPO. STATE should be 'open' or 'closed'.
 Only issues with KEYWORD in their description will be returned.
 NOTE: This is a legacy API call.")
-  (api-command (rel-path "/legacy/issues/search/~A/~A/~A/~A"
-                         owner repo state keyword) :method :get))
+  (cl-github:api-command (cl-github:rel-path "/legacy/issues/search/~A/~A/~A/~A"
+                                             owner repo state keyword) :method :get))
 
 (define-github-command search-repositories (keyword language page)
     (:docs "Search for repositories matching KEYWORD. If LANGUAGE is provided,
 only return repos written in the given language. If PAGE is provided, return
 the given PAGE of results. NOTE: This is a legacy API call.")
-  (api-command (rel-path "/legacy/repos/search/~A" keyword) :method :get
-               :body parameters))
+  (cl-github:api-command (cl-github:rel-path "/legacy/repos/search/~A" keyword)
+                         :method :get :body parameters))
 
 (define-github-command search-users (keyword page)
     (:docs "Search for users matching KEYWORD. If PAGE is provided, return the
 given PAGE of results. NOTE: This is a legacy API call.")
-  (api-command (rel-path "/legacy/users/search/~A" keyword) :method :get
-               :body parameters))
+  (cl-github:api-command (cl-github:rel-path "/legacy/users/search/~A" keyword)
+                         :method :get :body parameters))
